1a544d1cd8c3bea4f6a110adeb44f8b0ac5d4a0d
Michel Estermann <estermann.michel@gmail.com>
2017-07-09 22:39:15 +0200

    Fixed build script for conan_package_tools version on travis and appveyor.

--- build.py
+++ build.py
@@ -5,7 +5,7 @@ if __name__ == "__main__":
     builder = ConanMultiPackager(args="--build missing", username='meshell')
     builder.add_common_builds(pure_c=False)
     filtered_builds = []
-    for settings, options in builder.builds:
+    for settings, options, env_vars, build_requires in builder.builds:
         if (platform.system() == "Windows") and (settings["compiler"] == "gcc"):
             minGWOptions = options.copy()
             minGWOptions.update({"gmock:disable_pthreads": "True"})
